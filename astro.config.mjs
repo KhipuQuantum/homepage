bbfc0257a38f58ff68c3c19085c69d00a01c450e
KhipuQuantum <khipuquantum@gmail.com>
2024-05-06 11:15:15 -0500

    modified:   astro.config.mjs 	modified:   dockerfile 	modified:   package-lock.json 	modified:   package.json 	new file:   robots.txt.ts 	modified:   src/components/MainHead.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://khipuquantum.com',
+    integrations: [sitemap()],
     devToolbar: {
         enabled: false
     },
--- dockerfile
+++ dockerfile
@@ -6,6 +6,8 @@ COPY . .
 
 RUN npm install
 
+RUN npm run build
+
 EXPOSE 4120
 
-CMD ["npm", "start"]
+CMD ["npm", "run", "preview"]
